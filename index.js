f84204ffb42b48c1c9ac5502a65fb1b662ecfb50
Arthur Gabriel <123274385+arthurgmv@users.noreply.github.com>
2024-06-15 13:34:38 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 // Detects Buttons
 let numberOfDrums = document.querySelectorAll(".drum").length;
 
+
 for (let i = 0; i < numberOfDrums; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
         let buttonInnerHTML = this.innerHTML;
